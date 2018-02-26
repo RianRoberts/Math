61d027b07e490b1e1dea23a8f55fdd0806842804
Rian Roberts <rr923979@wtstudents.wtamu.edu>
2018-02-26 11:42:27 -0600

    commit

---
+++ Models/MathOperation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace MathProgram.Models{
+public class MathOperation
+{
+    public float leftOperand { get; set; }
+    public float RightOperand { get; set; }
+    public string Operator { get; set; }
+    public float Result { get; set; }
+}
+}
